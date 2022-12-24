4ded3b20ff96193688a96196c06ff75fc111b967
Vladimir <mr.mystory70@gmail.com>
2022-12-24 13:13:16 +0300

    Last commit

---
+++ date.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$tasks = [
+    [
+        "task" => "Собеседование в IT компании",
+        "deadline" => "01.02.2023",
+        "category" => "Работа",
+        "done" => false
+    ],
+    [
+        "task" => "Выполнить тестовое задание",
+        "deadline" => "25.12.2022",
+        "category" => "Работа",
+        "done" => false
+    ],
+    [
+        "task" => "Сделать задание первого раздела",
+        "deadline" => "24.12.2022",
+        "category" => "Учеба",
+        "done" => true
+    ],
+    [
+        "task" => "Встреча с другом",
+        "deadline" => "27.12.2022",
+        "category" => "Входящие",
+        "done" => false
+    ],
+    [
+        "task" => "Купить корм для кота",
+        "deadline" => null,
+        "category" => "Домашние дела",
+        "done" => false
+    ],
+    [
+        "task" => "Заказать пиццу",
+        "deadline" => null,
+        "category" => "Домашние дела",
+        "done" => false
+    ],
+
+];
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Hello, world!";
-
-$some_var = false;
-
-if ($some_var == false) {
-    echo 'false';
-}
